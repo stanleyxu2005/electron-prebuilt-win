1b1eae2a9fc802cdba4ba10a657377520851f9bb
Qian Xu <sx.away@googlemail.com>
2018-06-03 18:41:16 +0800

    moved my local build stuff into build_local.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 DIST_DIR=dist
 export GYP_MSVS_VERSION=2015
-#export GYP_ARGS="--disturl=http://127.0.0.1/pub/iojs"
 
 \rm -rf ${DIST_DIR}
 ./vendors/7z/7z.exe x vendors/electron-*.zip -o${DIST_DIR}
