720453e8b8d16b212e9bc8de6697b5ff67939c18
Qian Xu <sx.away@googlemail.com>
2019-01-02 18:03:37 +0800

    for 4.0.0 the header files are split into more chunks

--- get_iojs.sh
+++ get_iojs.sh
@@ -9,13 +9,20 @@ fi
 DIST_DIR=vendors
 IOJS_DIR=iojs
 if [ ! -d ${IOJS_DIR} ]; then
+  mkdir -p ${IOJS_DIR}/x64
   mkdir -p ${IOJS_DIR}/win-x64
   mkdir -p ${IOJS_DIR}/win-x86
   wget https://atom.io/download/electron/v${VER}/SHASUMS256.txt -O ${IOJS_DIR}/SHASUMS256.txt
   wget https://atom.io/download/electron/v${VER}/iojs-v${VER}.tar.gz -O ${IOJS_DIR}/iojs-v${VER}.tar.gz
   wget https://atom.io/download/electron/v${VER}/iojs-v${VER}-headers.tar.gz -O ${IOJS_DIR}/iojs-v${VER}-headers.tar.gz
+  wget https://atom.io/download/electron/v${VER}/node-v${VER}.tar.gz -O ${IOJS_DIR}/node-v${VER}.tar.gz
+  wget https://atom.io/download/electron/v${VER}/node-v${VER}-headers.tar.gz -O ${IOJS_DIR}/node-v${VER}-headers.tar.gz
+  wget https://atom.io/download/electron/v${VER}/node.lib -O ${IOJS_DIR}/node.lib
+  wget https://atom.io/download/electron/v${VER}/x64/node.lib -O ${IOJS_DIR}/x64/node.lib
   wget https://atom.io/download/electron/v${VER}/win-x64/iojs.lib -O ${IOJS_DIR}/win-x64/iojs.lib
+  wget https://atom.io/download/electron/v${VER}/win-x64/node.lib -O ${IOJS_DIR}/win-x64/node.lib
   wget https://atom.io/download/electron/v${VER}/win-x86/iojs.lib -O ${IOJS_DIR}/win-x86/iojs.lib
+  wget https://atom.io/download/electron/v${VER}/win-x86/node.lib -O ${IOJS_DIR}/win-x86/node.lib
   ZIP_FILE=${DIST_DIR}/iojs.zip
   \rm -f ${ZIP_FILE} && zip -r ${ZIP_FILE} ${IOJS_DIR}
   echo "iojs (${VER}) header files have been saved as ${ZIP_FILE}"
